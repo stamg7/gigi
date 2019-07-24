51201965f17b950dff104ed7c4dda05a5bcc2f28
stamg7 <stam.nick.1586@gmail.com>
2019-07-24 18:57:18 +0300

    Update

--- public/script.js
+++ public/script.js
@@ -2,23 +2,27 @@ const socket = io()
 const $form = document.querySelector('form')
 const $messages_w = document.querySelector('.messages_w')
 const $messageTemplate = document.querySelector('#message-template').innerHTML
+const $message_wrapper = document.querySelector('.message_wrapper')
+const $messageTemplateForColor = document.querySelector('#message-template').firstChild
+
+const messageColor = document.cookie.split('=')[1]
 
 $form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', {message , messageColor})
 })
 
-socket.on('message', (message) => {
-    console.log(message)
+socket.on('message', (msg) => {
 
     const html = Mustache.render($messageTemplate, {
-        message
+        msg
     })
 
     $messages_w.insertAdjacentHTML('beforeend', html)
+
 })
 
 console.log(socket.Nname)
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const mustache = require('mustache-express')
 
 const server = http.createServer(app)
 const io = socketio(server)
@@ -11,13 +12,21 @@ const io = socketio(server)
 // const viewPath = path.join(__dirname, '../public/views')
 // app.set('views', viewPath)
 app.use(express.static(path.join(__dirname, '../public')))
+// app.engine('html', mustache())
+// app.set('view engine', 'html')
+// const viewPath = path.join(__dirname, '../public/views')
+// app.set('views', viewPath)
+
+
+app.get("/:userName", (req, res) =>{
+    res.cookie('class', req.params.userName)
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 io.on('connection', (socket) => {
 
-  console.log(io.clients())
-
-  socket.on('sendMessage' , (message) => {
-    io.emit('message', message)
+  socket.on('sendMessage' , (msg) => {
+    io.emit('message', msg)
   })
 
 })
