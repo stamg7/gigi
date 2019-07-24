40566b5867981bace502d129cf5bcc7d7ab4c249
stamg7 <stam.nick.1586@gmail.com>
2019-07-24 19:57:36 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -11,16 +11,35 @@ const io = socketio(server)
 // app.set('view engine', 'hbs')
 // const viewPath = path.join(__dirname, '../public/views')
 // app.set('views', viewPath)
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public/nonViews')))
 // app.engine('html', mustache())
 // app.set('view engine', 'html')
 // const viewPath = path.join(__dirname, '../public/views')
 // app.set('views', viewPath)
 
+// app.get("", (req,res) => {
+//   console.log('sssss')
+//   res.sendFile(path.join(__dirname, '../public/404.html'))
+// })
 
-app.get("/:userName", (req, res) =>{
-    res.cookie('class', req.params.userName)
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get("/", (req,res) => {
+  console.log('sssssddddd')
+  res.sendFile(path.join(__dirname, '../public/views/404.html'))
+})
+
+app.get("/:user", (req, res) =>{
+
+    if(req.params.user.toString() === 'gigi' || req.params.user.toString() === 'stam' ){
+      res.cookie('class', req.params.user)
+      res.sendFile(path.join(__dirname, '../public/views/index.html'))
+    }else{
+      res.sendFile(path.join(__dirname, '../public/views/404.html'))
+    }
+    
+})
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, '../public/views/404.html'))
 })
 
 io.on('connection', (socket) => {
