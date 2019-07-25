f7fa85c759f98d157846fe4e794c7d9423626a3f
stamg7 <stam.nick.1586@gmail.com>
2019-07-25 18:21:56 +0300

    Update'

--- public/nonViews/script.js
+++ public/nonViews/script.js
@@ -4,19 +4,21 @@ const $messages_w = document.querySelector('.messages_w')
 const $messageTemplate = document.querySelector('#message-template').innerHTML
 const $message_wrapper = document.querySelector('.message_wrapper')
 const $messageTemplateForColor = document.querySelector('#message-template').firstChild
+const $msg_area = document.getElementById('msg_area')
 
 const messageColor = document.cookie.split('=')[1]
 
 $form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const message = e.target.elements.message.value
+    let message = e.target.elements.message.value
 
     socket.emit('sendMessage', {message , messageColor})
+
+    $msg_area.value = '';
 })
 
 socket.on('initialization', (arr) => {
-    console.log($messages_w.hasChildNodes())
     if(!$messages_w.hasChildNodes()){
         let msg = {}
         for(let el of arr){
@@ -30,6 +32,7 @@ socket.on('initialization', (arr) => {
         }
     
     }
+    $messages_w.scrollTop = $messages_w.scrollHeight
 })
 
 socket.on('message', (msg) => {
@@ -39,7 +42,22 @@ socket.on('message', (msg) => {
     })
 
     $messages_w.insertAdjacentHTML('beforeend', html)
-
-    console.log($messages_w.hasChildNodes())
+    autoscroll()
 
 })
+
+const autoscroll = () => {
+
+    const $newMessage = $messages_w.lastElementChild
+    console.log($messages_w.scrollHeight)
+    console.log($newMessage.offsetHeight)
+    console.log($messages_w.scrollTop)
+    console.log($messages_w.offsetHeight)
+    $a = parseInt(getComputedStyle($newMessage).marginBottom)
+    console.log($a)
+
+    if($messages_w.scrollHeight - $newMessage.offsetHeight - 5 <= $messages_w.scrollTop + $messages_w.offsetHeight){
+        $messages_w.scrollTop = $messages_w.scrollHeight
+    }
+
+}
