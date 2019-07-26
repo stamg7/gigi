3f3df80035cf6a3a9a2e37ebe22c011303e43f72
stamg7 <stam.nick.1586@gmail.com>
2019-07-26 16:53:16 +0300

    Upd

--- public/nonViews/script.js
+++ public/nonViews/script.js
@@ -9,7 +9,7 @@ const $msg_area = document.getElementById('msg_area')
 const messageColor = document.cookie.split('=')[1]
 
 $msg_area.addEventListener('focus', () => {
-    setTimeout(() => {$messages_w.scrollTop = $messages_w.scrollHeight * 4}, 300)
+    setTimeout(() => {$messages_w.scrollTop = $messages_w.scrollHeight * 4}, 500)
 })
 
 $form.addEventListener('submit', (e) => {
